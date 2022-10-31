7002906e2ff89a8491d8d5824d8c03ae72f46f90
hw commit <demaniks@yahoo.com>
2022-10-31 23:07:13 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        //task 1
+        int comingOfAge = 22;
+        if (comingOfAge >= 18) {
+            System.out.println("Поздравляем Вы совершеннолетний!");
+        }
+        if (comingOfAge <= 18) {
+            System.out.println("Ваш возраст для совершенолетия еще не наступил");
+        }
     }
 }
